2ad0f4d2ede1cfdf93b31b05d3eb3c3a32561124
Laura Spieker <ljspiek@gmail.com>
2020-01-14 19:34:33 -0600

    Button test completed

--- src/components/button.js
+++ src/components/button.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default function Button(props) {
+    console.log(props)
+  
     return <button onClick={() => props.onClick()}>Click me!</button>;
 }
+
+// const wrapper = shallow(<Button />);
+
+// expect(wrapper.find('.clicks-0').length).to.equal(1);
+// wrapper.find('a').simulate('click');
+// expect(wrapper.find('clicks-1').length).to.equal(1);
+
--- src/components/button.test.js
+++ src/components/button.test.js
@@ -5,7 +5,9 @@ import Button from './button';
 
 describe('<Button />', () => {
     it('Should call props.onClick when the button is clicked', () => {
-        const wrapper = shallow(<Button />);
+        const mockCallBack = jest.fn();
+        const wrapper = shallow(<Button onClick={mockCallBack} />);
         wrapper.find('button').simulate('click');
+        expect(mockCallBack.mock.calls.length).toEqual(1);
     });
 });
